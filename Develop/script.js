76e744139ceda1fb4082d134cb23994f19407c04
Liz DiTullio <lizdit72296@gmail.com>
2022-03-21 22:25:02 -0400

    added random generating feature

--- Develop/script.js
+++ Develop/script.js
@@ -6,65 +6,56 @@ var specialChars = "~`! @#$%^&*()_-+={[}]|\:;'<,>.?/";
 
 var allCharacters = "";
 
-var characterLength = function(charLength) {
-  charLength = window.prompt("How many characters would you like?");
+var generatePassword = function() {
+  var charLength = window.prompt("How many characters would you like?");
 
   if(charLength < 8 || charLength > 128) {
     window.alert("Please enter a number between 8 and 128.")
-    characters();
+    generatePassword();
     
   } else {
-   characterPrompts();
+        var lowerCaseConfirm = window.confirm("Would  you like to include lower case letters?");
+
+        var upperCaseConfirm = window.confirm("Would  you like to include upper case letters?");
+      
+        var numbersConfirm = window.confirm("Would  you like to include numbers?");
+      
+        var specialCharConfirm = window.confirm("Would  you like to include sepcial characters?");
+      
+      
+        if (lowerCaseConfirm == true) {
+          allCharacters = allCharacters + lowerCase;
+        } else {
+          return allCharacters;
+        }
+      
+        if (upperCaseConfirm == true) {
+          allCharacters = allCharacters + upperCase;
+        } else {
+          return allCharacters;
+        }
+      
+        if (numbersConfirm == true) {
+          allCharacters = allCharacters + numbers;
+        } else {
+         return allCharacters;
+        }
+
+        if (specialCharConfirm == true) {
+          allCharacters = allCharacters + specialChars;
+        } else {
+          return allCharacters;
+        }
+    console.log(allCharacters); 
       }
-  }
-
-var characterPrompts = function() {
-    
-    var lowerCaseConfirm = window.confirm("Would  you like to include lower case letters?");
-
-    var upperCaseConfirm = window.confirm("Would  you like to include upper case letters?");
-  
-    var numbersConfirm = window.confirm("Would  you like to include numbers?");
-  
-    var specialCharConfirm = window.confirm("Would  you like to include sepcial characters?");
-  
-  
-    if (lowerCaseConfirm == true) {
-      allCharacters = allCharacters + lowerCase;
-    } else {
-      allCharacters;
-    }
-  
-    if (upperCaseConfirm == true) {
-      allCharacters = allCharacters + upperCase;
-    } else {
-      allCharacters;
-    }
-  
-    if (numbersConfirm == true) {
-      allCharacters = allCharacters + numbers;
-    } else {
-      allCharacters;
-    }
-
-    if (specialCharConfirm == true) {
-      allCharacters = allCharacters + lowerCase;
-    } else {
-      allCharacters;
-    }
-  }
-
-var generatePassword = function() {
-    for(var i = 0; i < charLength; i++) {
-      var value = Math.floor(Math.random() + allCharacters.length);
-      allCharacters[value];
-      var password = password +  allCharacters[Math.random()]
-      console.log(password);
-    }
-}
-
-characterLength();
-generatePassword();
+          for(var i = 0; i < charLength; i++) {
+              var value = Math.floor(Math.random() * allCharacters.length);
+              var generatedPass = generatedPass +   allCharacters[value];
+            }
+            console.log(allCharacters[value]);
+            console.log(generatedPass);
+            return generatedPass;
+            }
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -73,7 +64,9 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  passwordText.textContent = password;
 }
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+writePassword();
